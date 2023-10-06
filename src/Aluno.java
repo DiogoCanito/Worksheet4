d0f3509dc44b03de7acabb56fbbdfe991776e7bb
DiogoCanito <canitodiogo@gmail.com>
2023-10-06 10:14:24 +0100

    start project

---
+++ src/Aluno.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Aluno {
+
+    private String nome;
+    private int num;
+
+    private Date data;
+
+    private double paga;
+
+    public Aluno(String n, int nu) {
+        this.nome = n;
+        this.num = nu;
+    }
+
+}
