207d944c682532183e296fa9c8d127ea162c5b65
DiogoCanito <canitodiogo@gmail.com>
2023-10-06 10:35:33 +0100

    Aluno classe com gets, sets e ToString versão 2

--- src/Aluno.java
+++ src/Aluno.java
@@ -14,4 +14,45 @@ public class Aluno {
         this.num = nu;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public double getPaga() {
+        return paga;
+    }
+
+    public void setPaga(double paga) {
+        this.paga = paga;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", num=" + num +
+                ", data=" + data +
+                ", paga=" + paga +
+                '}';
+    }
 }
